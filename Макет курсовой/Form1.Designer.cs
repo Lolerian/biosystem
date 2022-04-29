befcde51b8aeb46b2647649540df025c3671af21
USER <USER@DESKTOP-5PQ6JU0>
2022-04-29 09:31:51 +0300

    Добавьте файлы проекта.

---
+++ Макет курсовой/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Макет_курсовой
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(106, 45);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tabControl1.Location = new System.Drawing.Point(840, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(294, 669);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(286, 640);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Создание";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(157, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Очистить поле";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(286, 640);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Сведенья";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество жертв";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(286, 640);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Графики";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(840, 669);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 669);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Макет курсовой/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Макет_курсовой
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pole p = new pole(pictureBox2, pictureBox2.Width, pictureBox2.Height);
+        }
+        public Victim[] victims = new Victim[5];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                victims[i] = new Victim();
+            }
+            pole.loadGrid();
+            //выключение кнопки после отрисовки поля
+            button1.Enabled = false;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            //pole.drawLudi();
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //вывод пользовательского значения жертв
+            ric.kolvoj = Convert.ToInt32(numericUpDown1.Value);
+            // временная штука для вывода количества
+            label2.Text = Convert.ToString(ric.kolvoj);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //очистка поля (типо обновление)
+            pole.gr.Clear(Color.White);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                victims[i].MoveVictim();
+            }
+            pictureBox2.Image = pole.bmp;
+        }
+    }
+    class ric
+    {
+        //статические значения для поля (заменить поле потом)
+        //переменная для количества жертв
+        public static int kolvoj;
+        public static Pen pole = new Pen(Color.LawnGreen);
+        public static Random random = new Random();
+        public static Pen jertvi = new Pen(Color.Black);
+        public static Pen whitePen = new Pen(Color.White);
+
+    }
+    class pole
+    {
+        public static int width = Screen.PrimaryScreen.Bounds.Width;
+        public static int height = Screen.PrimaryScreen.Bounds.Height;
+        public static PictureBox pic = new PictureBox();
+        public static Bitmap bmp = new Bitmap(width, height);
+        public static Graphics gr = Graphics.FromImage(bmp);
+
+        //public static int[,] MatricaPolia = new int[width,height];
+
+        public pole(PictureBox pic, int width, int height)
+        {
+            pole.pic = pic;
+            pole.width = width;
+            pole.height = height;
+
+        }
+        public static void loadGrid()
+        {
+            for (int i = 0; i < 1500/5; i++)
+            {
+                gr.DrawLine(ric.pole, i * 5, 0, i * 5, 880);
+            }
+            //ширина
+            for (int i = 0; i < 880/5; i++)
+            {
+                gr.DrawLine(ric.pole, 0, i * 5, 1500, i * 5);
+            }
+            pic.Image = bmp;
+        }
+    }
+    public class Victim
+    {
+        
+        public int randStartPosX = ric.random.Next(1300);
+        public int randStartPosY = ric.random.Next(1000);
+
+        public int x = 0;
+        public int y = 0;
+
+        public static int r = pole.width;
+        public static int c = pole.height;
+        public int[,] mainMatrix = new int[2000,2000];
+
+        public void MoveVictim()
+        {
+            int moveRotate = ric.random.Next(0,5);
+            if(moveRotate == 0)
+            {
+                x = x + 5;
+            }
+            if (moveRotate == 1)
+            {
+                x = x - 5;
+            }
+            if (moveRotate == 2)
+            {
+                y = y + 5;
+            }
+            if (moveRotate == 3)
+            {
+                y = y - 5;
+            }
+
+            if(mainMatrix[randStartPosX+x, randStartPosY+y] == 0)
+            {
+                mainMatrix[randStartPosX, randStartPosY] = 0;
+                pole.gr.DrawRectangle(ric.jertvi, randStartPosX+x, randStartPosY+y, 5, 5);
+            }
+            
+            if(mainMatrix[randStartPosX+x+5, randStartPosY+y] == 1)
+            {
+                mainMatrix[randStartPosX, randStartPosY] = 0;
+                pole.gr.DrawRectangle(ric.whitePen, randStartPosX+x+5, randStartPosY+y, 5, 5);
+            }
+            if (mainMatrix[randStartPosX+x-5, randStartPosY+y] == 1)
+            {
+                mainMatrix[randStartPosX, randStartPosY] = 0;
+                pole.gr.DrawRectangle(ric.whitePen, randStartPosX+x-5, randStartPosY+y, 5, 5);
+            }
+            if (mainMatrix[randStartPosX+x, randStartPosY+y-5] == 1)
+            {
+                mainMatrix[randStartPosX, randStartPosY] = 0;
+                pole.gr.DrawRectangle(ric.whitePen, randStartPosX+x, randStartPosY+y-5, 5, 5);
+            }
+            if (mainMatrix[randStartPosX+x, randStartPosY+y+5] == 1)
+            {
+                mainMatrix[randStartPosX, randStartPosY] = 0;
+                pole.gr.DrawRectangle(ric.whitePen, randStartPosX+x, randStartPosY+y+5, 5, 5);
+            }
+            //обновлять каждый раз или хранить прошлое движение в пред клетке
+        }
+    }
+}
